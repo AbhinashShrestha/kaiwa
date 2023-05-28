2e69c78417d44013a5577a3ffe3dc188e2586432
Abhinash Shrestha <mr.indestructible010@gmail.com>
2023-05-28 15:52:56 +0545

    Auth Setup Done

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import AuthForm from "./components/AuthForm";
+
 export default function Home() {
     return (
         <div className="
@@ -8,10 +11,26 @@ export default function Home() {
             py-12
             sm:px-6
             lg:px-8
-            bg-gray-100
-
+            bg-teal-50
         ">
-
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                <Image 
+                    alt="Messaging app logo"
+                    height="80"
+                    width="80"
+                    className="mx-auto w-auto"
+                    src="/images/logo.png"
+                />
+                <h2 className="mt-6
+                    text-center
+                    text-3xl
+                    font-bold
+                    tracking-tight
+                    text-gray-900">
+                    Sign in to your Account
+                </h2>
+            </div>
+            <AuthForm />
         </div>
     )
   }
